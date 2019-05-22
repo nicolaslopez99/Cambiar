453b96aa289474c04076c0dc0cba13600094637c
nicolaslopez99 <42530363+nicolaslopez99@users.noreply.github.com>
2019-05-21 22:17:08 -0500

    Add files via upload

---
+++ l.bat
@@ -0,0 +1,35 @@
+setlocal enabledelayedexpansion 
+
+if exist á do(
+	for %%a in (*.*) do ( set file=%%a 
+		ren "!file!" "!file:á=a!"
+		)
+	)
+if exist ‚ do(
+	for %%a in (*.*) do ( set file=%%a 
+		ren "!file!" "!file:‚=e!"
+		)
+	)
+if exist ¡ do(
+	for %%a in (*.*) do ( set file=%%a 
+		ren "!file!" "!file:¡=i!"
+		)
+	)
+
+if exist ¢ do(
+	for %%a in (*.*) do ( set file=%%a 
+		ren "!file!" "!file:¢=o!"
+		)
+	)
+if exist £ do(
+	for %%a in (*.*) do ( set file=%%a 
+		ren "!file!" "!file:£=u!"
+		)
+	)
+if exist ¤ do(
+	for %%a in (*.*) do ( set file=%%a 
+		ren "!file!" "!file:¤=n!"
+		)
+	)
+pause 
+exit
